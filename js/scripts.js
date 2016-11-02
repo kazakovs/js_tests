a0882c0630c2624783204dc31ae6edb00a71983f
kazakovs <kazakovs@users.noreply.github.com>
2016-11-02 12:48:07 +0100

    Add files via upload
    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+var c = document.getElementById("myCanvas");
+
+function fillCanvasWithHLines(canvas, numberLines){
+	ctx = canvas.getContext("2d");
+	w=canvas.width;
+	h=canvas.height;
+	step = h/numberLines;
+	ctx.beginPath();
+	for (i = 1; i <= numberLines; i++) {
+		ctx.moveTo(0, i*step-5);
+		ctx.lineTo(w, i*step-5);
+	}
+	ctx.stroke();
+}
+
+function fillCanvasWithVLines(canvas, numberLines){
+	ctx = canvas.getContext("2d");
+	w=canvas.width;
+	h=canvas.height;
+	step = w/numberLines;
+	ctx.beginPath();
+	for (i = 1; i <= numberLines; i++) {
+		ctx.moveTo(i*step-5, 0);
+		ctx.lineTo(i*step-5, h);
+	}
+	ctx.stroke();
+}
+
+function redraw(){
+	ctx = c.getContext("2d");
+	ctx.clearRect(0,0, c.width, c.height);
+	if(document.getElementById('h_chkbox').checked){
+		fillCanvasWithHLines(c, 10);
+	}
+	if(document.getElementById('v_chkbox').checked){
+		fillCanvasWithVLines(c, 10);
+	}
+}
+
